f334eef6f77521c3855ff9d4bf99e6740bdb505a
Piotrek Kochański <mailpiotrk@gmail.com>
2017-11-27 13:06:22 +0100

    change tabs to spaces, change comments to english

--- pzed/CMemory.cpp
+++ pzed/CMemory.cpp
@@ -6,56 +6,56 @@
 
 DWORD CMemory::getPId(const std::wstring& processName)
 {
-	DWORD pid;
-	if (getPId(processName, pid) { return pid; }
-	// tu proelo throw
+    DWORD pid;
+    if (getPId(processName, pid) { return pid; }
+    // throw here
 }
 
 bool CMemory::getPId(const std::wstring& processName, DWORD& pid)
 {
-	HANDLE hProcessSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	PROCESSENTRY32 processEntry32;
-	processEntry32.dwSize = sizeof(PROCESSENTRY32);
+    HANDLE hProcessSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    PROCESSENTRY32 processEntry32;
+    processEntry32.dwSize = sizeof(PROCESSENTRY32);
 
-	if (Process32First(hProcessSnapshot, &processEntry32))
-	{
-		do
-		{
-			if (std::wstring(processEntry32.szExeFile) == processName)
-			{
-				properlyCloseHandle(hProcessSnapshot);
-				pid = processEntry32.th32ProcessID;
-				return true;
-			}
-		} while (Process32Next(hProcessSnapshot, &processEntry32));
-	}
-	properlyCloseHandle(hProcessSnapshot);
-	return false;
+    if (Process32First(hProcessSnapshot, &processEntry32))
+    {
+        do
+        {
+            if (std::wstring(processEntry32.szExeFile) == processName)
+            {
+                properlyCloseHandle(hProcessSnapshot);
+                pid = processEntry32.th32ProcessID;
+                return true;
+            }
+        } while (Process32Next(hProcessSnapshot, &processEntry32));
+    }
+    properlyCloseHandle(hProcessSnapshot);
+    return false;
 }
 
 DWORD CMemory::getModule(const std::wstring& moduleName, DWORD pid)
 {
-	DWORD dwAddress;
-	if (getModule(moduleName, pid, dwAddress) { return dwAddress; }
-	// tu proelo throw
+    DWORD dwAddress;
+    if (getModule(moduleName, pid, dwAddress) { return dwAddress; }
+    // throw here
 }
 
 bool CMemory::getModule(const std::wstring& moduleName, DWORD pid, DWORD& dwAddress)
 {
-	HANDLE h_moduleSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid);
-	MODULEENTRY32 moduleEntry32;
-	moduleEntry32.dwSize = sizeof(MODULEENTRY32);
+    HANDLE h_moduleSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid);
+    MODULEENTRY32 moduleEntry32;
+    moduleEntry32.dwSize = sizeof(MODULEENTRY32);
 
-	if (Module32First(h_moduleSnapshot, &moduleEntry32))
-	{
-		do
-		{
-			if (moduleEntry32.szModule == moduleName)
-			{
-				dwAddress = reinterpret_cast<DWORD>(moduleEntry32.modBaseAddr);
-				return true;
-			}
-		} while (Module32Next(h_moduleSnapshot, &moduleEntry32));
-	}
-	return false;
+    if (Module32First(h_moduleSnapshot, &moduleEntry32))
+    {
+        do
+        {
+            if (moduleEntry32.szModule == moduleName)
+            {
+                dwAddress = reinterpret_cast<DWORD>(moduleEntry32.modBaseAddr);
+                return true;
+            }
+        } while (Module32Next(h_moduleSnapshot, &moduleEntry32));
+    }
+    return false;
 }
--- pzed/CMemory.h
+++ pzed/CMemory.h
@@ -6,65 +6,72 @@
 
 #include <Windows.h>
 
-// funkcja konwertuj¹ca z stringa na wstringa
-// wersje funkcji przyjmuj¹ce wchar*, stringi, i char*
+// TODO: add fuctions accepting wchar*,
+// char* and strings
+
+// TODO: add throws and exceptions handling
 
 class CMemory
 {
 public:
-	CMemory() = delete;
+    CMemory() = delete;
 
-	static bool isValidHandle(HANDLE h) { return h != INVALID_HANDLE_VALUE; }
-	static bool isValidProcessHandle(HANDLE h) { return isValidHandle(h) && h > 0; }
-	static bool properlyCloseHandle(HANDLE h)
-	{
-		auto const a = CloseHandle(h);
-		assert(a && "handle not closed properly");
-		return a;
-	}
+    static bool isValidHandle(HANDLE h) { return h != INVALID_HANDLE_VALUE; }
+    static bool isValidProcessHandle(HANDLE h) { return isValidHandle(h) && h > 0; }
+    static bool properlyCloseHandle(HANDLE h)
+    {
+        auto const a = CloseHandle(h);
+        assert(a && "handle not closed properly");
+        return a;
+    }
 
-	static DWORD getPId(const std::wstring& processName);
-	static bool getPId(const std::wstring& processName, DWORD& pid);
+    /**
+     * get process id of said process
+     * @param name of a process
+     * @ret process id
+     */
+    static DWORD getPId(const std::wstring& processName);
+    static bool getPId(const std::wstring& processName, DWORD& pid);
 
-	// jak na razie otwiera tylko z jednymi uprawnieniami, tylko tyle potrzebujemy,
-	// ale mo¿e lepiej to zmieniæ, daæ mo¿liwoœæ wiêkszego manewru, enum z mo¿liwymi wartoœciami?
-	static HANDLE getPHandle(DWORD pid) { return OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid); }
-	static void getPHandle(DWORD pid, HANDLE& h) { h = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid); }
+    // for now opens only with one access rights, only one needed for now
+    // maybe change in future, have more options, add enumerator with access rights?
+    static HANDLE getPHandle(DWORD pid) { return OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid); }
+    static void getPHandle(DWORD pid, HANDLE& h) { h = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid); }
 
-	static DWORD getModule(const std::wstring& moduleName, DWORD pid);
-	static bool getModule(const std::wstring& moduleName, DWORD pid, DWORD& dwAddress);
+    static DWORD getModule(const std::wstring& moduleName, DWORD pid);
+    static bool getModule(const std::wstring& moduleName, DWORD pid, DWORD& dwAddress);
 
-	// mo¿e napisaæ wersje przyjmuj¹c¹ value by value (xD) aby unikn¹æ kopiowania primitywnych typów,
-	// resztê const_castowaæ? rozwi¹zanie doœæ patologiczne
-	// mo¿e const& w ogólnie niepotrzebne czy piszemy tu kiedykolwiek naprawdê du¿e rzeczy?
-	template<typename T>
-	static bool write(HANDLE hProcess, DWORD dwAddress, const T& value)
-	{
-		static_assert(std::is_trivially_copyable<T>::value, "Invalid RPM/WPM type");
+    // maybe add this function getting param value by value for primitive types,
+    // all others would probably need to be const_casted (pretty patological)
+    // maybe const ref not needed at all, are we passing really big things here?
+    template<typename T>
+    static bool write(HANDLE hProcess, DWORD dwAddress, const T& value)
+    {
+        static_assert(std::is_trivially_copyable<T>::value, "Invalid RPM/WPM type");
 
-		if (isValidProcessHandle(hProcess))
-			return WriteProcessMemory(hProcess, reinterpret_cast<LPVOID>(dwAddress), &value, sizeof(T), NULL);
-		return false;
-	}
+        if (isValidProcessHandle(hProcess))
+            return WriteProcessMemory(hProcess, reinterpret_cast<LPVOID>(dwAddress), &value, sizeof(T), NULL);
+        return false;
+    }
 
-	template<typename T>
-	static bool read(HANDLE hProcess, DWORD dwAddress, T& value)
-	{
-		static_assert(std::is_trivially_copyable<T>::value, "Invalid RPM/WPM type");
+    template<typename T>
+    static bool read(HANDLE hProcess, DWORD dwAddress, T& value)
+    {
+        static_assert(std::is_trivially_copyable<T>::value, "Invalid RPM/WPM type");
 
-		SIZE_T bytesRead;
+        SIZE_T bytesRead;
 
-		if (isValidProcessHandle(hProcess))
-			return ReadProcessMemory(hProcess, reinterpret_cast<LPCVOID>(dwAddress), &value, sizeof(T), &bytesRead) && bytesRead == sizeof(T);
-		return false;
-	}
+        if (isValidProcessHandle(hProcess))
+            return ReadProcessMemory(hProcess, reinterpret_cast<LPCVOID>(dwAddress), &value, sizeof(T), &bytesRead) && bytesRead == sizeof(T);
+        return false;
+    }
 
-	// nie jestem pewien tej funcji, na razie zarys
-	template<typename T>
-	static T read(HANDLE hProcess, DWORD dwAddress)
-	{
-		T value = T();
-		if (read<T>(hProcess, dwAddress, value) { return value; }
-		// throw
-	}
+    // not sure about this one
+    template<typename T>
+    static T read(HANDLE hProcess, DWORD dwAddress)
+    {
+        T value = T();
+        if (read<T>(hProcess, dwAddress, value) { return value; }
+        // throw
+    }
 };
